900bae52ee3451d02c985c3229f3931b7b8c3aaf
christopher <chriscous00@gmail.com>
2023-10-18 16:50:31 -0400

    first project

---
+++ Starting-React-1/src/App.js
@@ -0,0 +1,13 @@
+import Concepts from './components/Concepts';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+          <Header />
+          <Concepts />
+    </div>
+  );
+}
+
+export default App;
