99ee185e7669280089262b5b987f65fb96882de5
christopher <chriscous00@gmail.com>
2023-10-23 09:32:48 -0400

    advanced redux

---
+++ Advanced-Redux-React/src/components/Shop/Products.js
@@ -0,0 +1,29 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const DUMMY_PRODUCTS = [
+  { id: 'p1', price: 6, title: 'my first book', description: 'first book ever wrote' },
+  { id: 'p2', price: 5, title: 'my second book', description: 'second book ever wrote' }
+]
+
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
